f3e502f493fff8d1bd2a6ed130747f387d7a2a2f
vicentem <vicente.martinez-gomez@openmarket.com>
2019-03-14 17:41:05 -0600

    Init steps for tutorial

--- app/src/main/java/com/go/paydo/adapters/OperationsAdapter.kt
+++ app/src/main/java/com/go/paydo/adapters/OperationsAdapter.kt
@@ -1,8 +1,16 @@
 package com.go.paydo.adapters
 
-class OperationsAdapter {
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class OperationsAdapter(context : Context) : RecyclerView.Adapter<OperationsAdapter.OperationViewHolder>()  {
 
     /**
      * TODO(14): Create an adapter that works with a RecyclerView
      */
+
+    inner class OperationViewHolder (view : View) : RecyclerView.ViewHolder(view){
+
+    }
 }
