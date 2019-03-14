6d97a192777f033ca4f8bd6fd70bf27624aa838d
vicentem <vicente.martinez-gomez@openmarket.com>
2019-03-14 17:15:13 -0600

    Init steps for tutorial

--- app/src/main/java/com/go/paydo/viewmodel/OperationsViewModel.kt
+++ app/src/main/java/com/go/paydo/viewmodel/OperationsViewModel.kt
@@ -1,8 +1,20 @@
 package com.go.paydo.viewmodel
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
 
 //TODO(8): Make this a view model by extending AndroidViewModel. Match their constructors
 class OperationsViewModel {
+
+    private var parentJob = Job()
+
+    private val coroutineContext : CoroutineContext
+        get() = parentJob + Dispatchers.Main
+
+    private val scope = CoroutineScope(coroutineContext)
 
     //TODO(9): Make a variable to hold the OperationsRepository
 
@@ -10,7 +22,7 @@ class OperationsViewModel {
 
     //TODO(11): Use the 'init' block to initialize the previous step
 
-    //TODO(12 - Optional): Use a Job and a CoroutineContext to enhance lifecycle and prevent memory leaks
+    //TODO(12 - Provided): Use a Job and a CoroutineContext to enhance lifecycle and prevent memory leaks
 
     //TODO(13): Create at least 1 insert operation
 
